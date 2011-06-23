708a33bfee5171363515ad5de336694a85612725
Toshi MARUYAMA <marutosijp2@gmail.com>
2011-06-23 19:59:56 +0900

    Rails3.1: comment out "exempt_from_layout" at application_controller.rb.
    --HG--
    branch : rails3-straight-20110822-2-hg-git
    extra : rebase_source : 1b7ae4cf19a640ef3578cc954c34932bab7bc716

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,13 +24,14 @@ class ApplicationController < ActionController::Base
   include Redmine::I18n
 
   layout 'base'
-  exempt_from_layout 'builder', 'rsb'
+  # exempt_from_layout 'builder', 'rsb'
   
   protect_from_forgery
   def handle_unverified_request
     super
     cookies.delete(:autologin)
   end
+
   # Remove broken cookie after upgrade from 0.8.x (#4292)
   # See https://rails.lighthouseapp.com/projects/8994/tickets/3360
   # TODO: remove it when Rails is fixed
