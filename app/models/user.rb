6b9a1f17f2fb2f1efa1b2ae862582df3b89b5763
Jeremy Kemper <jeremy@bitsweat.net>
2011-08-29 15:36:55 +0900

    Rails3.1: model: pending: replace deprecate 'before_validation' instance method to declared validation method at AnonymousUser model.
    --HG--
    branch : rails3-straight-20110918-18-hg-git
    extra : rebase_source : 90aa7b48b6c7bc7bf4722b76ede0af4219a9f5d8

--- app/models/user.rb
+++ app/models/user.rb
@@ -593,8 +593,9 @@ class User < Principal
 end
 
 class AnonymousUser < User
+  before_validation :ensure_single_anonymous_user, :on => :create
 
-  def validate_on_create
+  def ensure_single_anonymous_user
     # There should be only one AnonymousUser in the database
     errors.add_to_base 'An anonymous user already exists.' if AnonymousUser.find(:first)
   end
