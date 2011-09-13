1300917e802baa0bfe9d290b960d4a0da2c85b4f
Toshi MARUYAMA <marutosijp2@gmail.com>
2011-09-13 22:46:29 +0900

    Rails3.1: use asset
    --HG--
    branch : rails3-straight-20110918-18-hg-git
    extra : rebase_source : 6c665d77cbf8ca7783ccf9107b8149340338eda2

--- config/application.rb
+++ config/application.rb
@@ -45,6 +45,9 @@ module Redmine
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
   end
 end
 
